32cd7487bd3668ec47baa2a13f962c839773e940
Vaibhav <vsahay28@gmail.com>
2020-10-08 05:10:12 -0400

    Added CountingBloomFilter implementation

--- Bloomfilter-Implementation/src/ApplicationRunner.java
+++ Bloomfilter-Implementation/src/ApplicationRunner.java
@@ -2,6 +2,7 @@
 public class ApplicationRunner {
 
   public static void main(String[] args) {
+    System.out.println("********BLOOM FILTER IMPLEMENTATION***********");
     BloomFilter bf = new BloomFilter(10000, 7, 1000);
 
     // set A
@@ -14,6 +15,15 @@ public class ApplicationRunner {
     bf.generateElements();
     bf.lookupElements();
     System.out.println("Found in set B = " + bf.numFound);
+
+    System.out.println("********COUNTING BLOOM FILTER IMPLEMENTATION***********");
+    CountingBloomFilter cbf = new CountingBloomFilter(1000, 10000, 7, 500, 500);
+    cbf.generateElements();
+    cbf.encodeElements();
+    cbf.removeElements();
+    cbf.addElements();
+    cbf.lookupElements();
+    System.out.println("Found in Set A= " + cbf.numFound);
   }
 
 }
---
+++ Bloomfilter-Implementation/src/CountingBloomFilter.java
@@ -0,0 +1,114 @@
+import java.util.Random;
+
+public class CountingBloomFilter {
+  int numBits; // 10000
+  int numElements; // 1000
+  int numHashes; // 7
+  int[] s;
+  int[] elements;
+  int numFound;
+  int numRemoved; // 500
+  int numAdded; // 500
+  int[] counter;
+  int[] removed;
+  int[] original;
+  Random rand = new Random();
+
+  public CountingBloomFilter(int numElements, int numBits, int numHashes, int numRemoved,
+      int numAdded) {
+    this.numElements = numElements;
+    this.numBits = numBits;
+    this.numHashes = numHashes;
+    this.numRemoved = numRemoved;
+    this.numAdded = numAdded;
+    s = new int[numHashes];
+    counter = new int[numBits];
+    elements = new int[numElements];
+    removed = new int[numRemoved];
+    // original = new int[numElements - numRemoved];
+    generateKHashFunctions();
+  }
+
+  public void generateKHashFunctions() {
+    for (int i = 0; i < s.length; i++) {
+      s[i] = Math.abs(rand.nextInt());
+    }
+  }
+
+  public void generateElements() {
+    for (int i = 0; i < numElements; i++)
+      elements[i] = Math.abs(rand.nextInt());
+  }
+
+  public void encodeElements() {
+    for (int i = 0; i < elements.length; i++)
+      if (elements[i] > 0)
+        encode(elements[i]);
+  }
+
+  public boolean encode(int element) {
+    int[] hashes = new int[numHashes];
+    boolean flag = false;
+    for (int i = 0; i < numHashes; i++) {
+      hashes[i] = (s[i] ^ element) % numBits;
+    }
+    // increment the k counters
+    for (int i = 0; i < hashes.length; i++) {
+      int hash = hashes[i];
+      counter[hash]++;
+    }
+    return flag;
+  }
+
+  public void removeElements() {
+    for (int i = 0; i < numRemoved; i++) {
+      int index = Math.abs(rand.nextInt(elements.length));
+      removed[i] = elements[index];
+      remove(elements[index]);
+      elements[index] = 0;
+
+    }
+  }
+
+  private void remove(int element) {
+    for (int i = 0; i < numHashes; i++) {
+      int hash = (s[i] ^ element) % numBits;
+      counter[hash]--;
+    }
+  }
+
+  public void addElements() {
+    for (int i = 0; i < numAdded; i++) {
+      encode(Math.abs(rand.nextInt()));
+    }
+  }
+
+  /*
+   * private void add(int element) { for (int i = 0; i < numHashes; i++) { int hash = (s[i] ^
+   * element) % numBits; counter[hash]++; } }
+   */
+
+  public void lookupElements() {
+    for (int i = 0; i < elements.length; i++) {
+      if (elements[i] > 0)
+        lookup(elements[i]);
+    }
+  }
+
+  private void lookup(int element) {
+    int[] hashes = new int[numHashes];
+    for (int i = 0; i < numHashes; i++) {
+      hashes[i] = (s[i] ^ element) % numBits;
+    }
+
+    for (int i = 0; i < numHashes; i++) {
+      if (counter[hashes[i]] == 0) {
+        continue;
+      } else {
+        numFound++;
+        break;
+      }
+    }
+  }
+
+}
